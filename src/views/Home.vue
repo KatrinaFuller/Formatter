fd4da38fd0e4531d777c9749f6198873e6621b0d
Katrina Fuller <trinafuller@att.net>
2019-10-13 17:57:34 -0600

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <!-- title -->
+    <div class="row">
+      <div class="col">
+        <h1>Formatter</h1>
+      </div>
+    </div>
+    <!-- input area -->
+    <div class="row">
+      <div class="col-6 border">
+        <h4>Input</h4>
+        <form action>
+          <div class="form-group">
+            <!-- <label for="exampleFormControlTextarea1">Example textarea</label> -->
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="25"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="col-6 border">
+        <h4>Output</h4>
+        <form action>
+          <div class="form-group">
+            <!-- <label for="exampleFormControlTextarea1">Example textarea</label> -->
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="25"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
